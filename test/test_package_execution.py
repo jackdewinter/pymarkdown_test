e15e860d7fd507e93c31082f22e2f32ce5b63646
jackdewinter <jack.de.winter@outlook.com>
2023-05-13 19:52:43 -0700

    Adding support for python 3.10 (#3)
    python 3.10 support

--- test/test_package_execution.py
+++ test/test_package_execution.py
@@ -1,11 +1,27 @@
 """
 Tests to apply the pre-commit hook invocation of PyMarkdown.
 """
+import os
 import tempfile
+from typing import List
 
 import pytest
 
 from .util_helpers import UtilHelpers
+
+
+def __xx(expected_output_lines: List[str]) -> None:
+    if UtilHelpers.get_python_version().startswith("3.10."):
+        k = next(
+            (
+                i
+                for i, j in enumerate(expected_output_lines)
+                if j == "optional arguments:"
+            ),
+            -1,
+        )
+        assert k != -1, "Pre-version 3.10 behavior not found."
+        expected_output_lines[k] = "options:"
 
 
 @pytest.mark.packages
@@ -13,6 +29,8 @@ def test_package_one() -> None:
     """
     Test to make sure that we can install the package and run it without any arguments.
     """
+
+    print(os.environ.copy())
 
     # Arrange
     UtilHelpers.assert_pymarkdown_install_package_present()
@@ -22,6 +40,10 @@ def test_package_one() -> None:
     windows_output_lines = UtilHelpers.load_templated_output(
         "package_one", "windows_output_template"
     )
+    __xx(expected_output_lines)
+    for i, j in enumerate(expected_output_lines):
+        print(f"({i}):{j}")
+    __xx(windows_output_lines)
 
     with tempfile.TemporaryDirectory() as temporary_directory:
         execution_environment = UtilHelpers.install_pymarkdown_in_fresh_environment(
--- test/util_helpers.py
+++ test/util_helpers.py
@@ -61,6 +61,16 @@ class UtilHelpers:
     __package_extension = ".tar.gz"
 
     @staticmethod
+    def get_python_version() -> str:
+        """
+        Get a cleaned up python version.
+        """
+        current_python_version = sys.version
+        index = current_python_version.index("(")
+        current_python_version = current_python_version[:index].strip()
+        return current_python_version
+
+    @staticmethod
     def __get_github_key_token_from_environment() -> Optional[str]:
         """
         Query the environment for a access token to use.
@@ -305,9 +315,7 @@ class UtilHelpers:
         Create a pipenv lock file.
         """
 
-        current_python_version = sys.version
-        index = current_python_version.index("(")
-        current_python_version = current_python_version[:index].strip()
+        current_python_version = UtilHelpers.get_python_version()
 
         command_result = subprocess.run(
             [
